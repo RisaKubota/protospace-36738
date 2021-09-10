99cdebd2d524e9c7addb349a8e5e7b1ee66d9f3f
RisaKubota <89505889+RisaKubota@users.noreply.github.com>
2021-09-10 17:21:57 +0900

    バリデーション設定(prototype)

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,12 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  #with_options presence: true do~end 値が存在 (presence) しなければならないという制限を一括で設ける
+  with_options presence: true do
+    validates :title
+    validates :catch_copy
+    validates :concept
+    validates :image
+  end
 end
