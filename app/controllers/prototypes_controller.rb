55469f617977b34e89c579205a620ea54d45347c
RisaKubota <89505889+RisaKubota@users.noreply.github.com>
2021-09-11 15:14:46 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,9 +3,28 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+    #newメソッド：インスタンスを生成
   end
 
   def create
+    #Prototype.create(prototype_params)
+    #createメソッド：引数に渡されたデータを元にモデルのインスタンスを生成し、データベースに保存
+    @prototype = Prototype.new(prototype_params)
+    # new.html.erbでインスタンスを渡す必要があるため、newアクションと同じ変数にする
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new #new:new.html.erb(renderはビューファイルに遷移)
+    end
+  end
+
+  private  #Class外から呼び出し不可のメソッド/誤呼出防止/可読性向上
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
+    #params.require(model).permit(:key, :key)
+    #paramsが使用できるメソッド： requireメソッド / permitメソッド
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
+  # get 'prototypes/index' #初期設定で出てくるので消してOK
   root to: "prototypes#index"
   resources :prototypes, only: [:index, :new, :create]
   #onlyで必要なルーティングのみ設定することで可読性が上がる
