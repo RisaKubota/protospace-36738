6c7cf60e2bf31c6d965f8475d2de7cfe910942fd
RisaKubota <89505889+RisaKubota@users.noreply.github.com>
2021-09-13 16:02:04 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'prototypes/index' #初期設定で出てくるので消してOK
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
   #onlyで必要なルーティングのみ設定することで可読性が上がる
   #アプリケーションのデータ量も少なくなる（微量ではある）
 end
