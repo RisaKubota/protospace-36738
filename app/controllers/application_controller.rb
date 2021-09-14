ae6982886403db0d998bd36aa6a2b80258275e08
RisaKubota <89505889+RisaKubota@users.noreply.github.com>
2021-09-14 22:35:25 +0900

    修正(未ログイン)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!, only: [:new, :edit,:destroy]
+  #authenticate_user!:ログインしていなければログイン画面に遷移する
+  #only or except を使用し特定のアクションのみ指定することが可能
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
